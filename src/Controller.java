09ce8e30a5b7e122fdbbe3d753f1bfe3ca5d29a0
Ross Nelson <Rossn430@gmail.com>
2018-01-22 17:08:44 -0600

    Grid working

--- src/Controller.java
+++ src/Controller.java
@@ -1,17 +1,44 @@
 import javafx.application.Application;
+import javafx.scene.Scene;
+import javafx.scene.layout.AnchorPane;
+import javafx.scene.layout.Pane;
+import javafx.scene.paint.Color;
+import javafx.scene.shape.Rectangle;
 import javafx.stage.Stage;
 
 public class Controller extends Application{
 
+	private Pane pane;
+	private final int dimension = 10;
+	private final int scale = 50;
+	private final Map map = new Map();
+	
+	int[][] grid = map.getMap();
+	
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		launch(args);
 	}
 
 	@Override
-	public void start(Stage arg0) throws Exception {
-		// TODO Auto-generated method stub
-		
+	public void start(Stage stage) throws Exception {
+		pane = new AnchorPane();
+		Scene scene = new Scene(pane, 500, 500);
+		stage.setScene(scene);
+		stage.setTitle("PirateShipGame");
+		drawMap(dimension, scale, pane);
+		stage.show();
+	}
+	
+	// Draws the rectangles and fills them with the appropriate colors
+	public void drawMap(int d, int s, Pane p) {
+		for (int x = 0; x < d; x++) {
+			for (int y = 0; y < d; y++) {
+				Rectangle rect = new Rectangle(x*s, y*s, s, s);
+				rect.setStroke(Color.BLACK);
+				rect.setFill(Color.PALETURQUOISE);
+				p.getChildren().add(rect);
+			}
+		}
 	}
 
 }
--- src/Map.java
+++ src/Map.java
@@ -1,4 +1,15 @@
 
 public class Map {
-
+	
+	private int[][] map;
+	
+	// Returns the current values of the grid
+	public int[][] getMap() {
+		return map;
+	}
+	
+	// Constructor
+	public Map() {
+		map = new int[10][10];
+	}
 }
