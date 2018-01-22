960b9e34c193a36f67b4eba170f50bcf9542154a
Ross Nelson <Rossn430@gmail.com>
2018-01-22 17:52:36 -0600

    Ship controls without errors

--- src/Controller.java
+++ src/Controller.java
@@ -1,5 +1,8 @@
 import javafx.application.Application;
+import javafx.event.EventHandler;
 import javafx.scene.Scene;
+import javafx.scene.image.ImageView;
+import javafx.scene.input.KeyEvent;
 import javafx.scene.layout.AnchorPane;
 import javafx.scene.layout.Pane;
 import javafx.scene.paint.Color;
@@ -12,6 +15,7 @@ public class Controller extends Application{
 	private final int dimension = 10;
 	private final int scale = 50;
 	private final Map map = new Map();
+	private final Ship ship = new Ship(scale);
 	
 	int[][] grid = map.getMap();
 	
@@ -26,19 +30,58 @@ public class Controller extends Application{
 		stage.setScene(scene);
 		stage.setTitle("PirateShipGame");
 		drawMap(dimension, scale, pane);
+		initShip(pane);
 		stage.show();
+		startSailing(scene, pane);
 	}
 	
 	// Draws the rectangles and fills them with the appropriate colors
 	public void drawMap(int d, int s, Pane p) {
 		for (int x = 0; x < d; x++) {
 			for (int y = 0; y < d; y++) {
-				Rectangle rect = new Rectangle(x*s, y*s, s, s);
+				Rectangle rect = new Rectangle(x * s, y * s, s, s);
 				rect.setStroke(Color.BLACK);
 				rect.setFill(Color.PALETURQUOISE);
 				p.getChildren().add(rect);
 			}
 		}
 	}
+	
+	public void initShip(Pane p) {
+		ImageView i = ship.loadShipImage();
+		p.getChildren().add(i);
+	}
+	
+	public void drawShip(Pane p) {
+		ImageView i = ship.loadShipImage();
+		p.getChildren().set(p.getChildren().size() - 1, i);
+	}
+	
+	public void startSailing(Scene scene, Pane p) {
+		scene.setOnKeyPressed(new EventHandler<KeyEvent>() {
 
+			@Override
+			public void handle(KeyEvent e) {
+				switch(e.getCode()) {
+				case RIGHT:
+					ship.goEast();
+					break;
+				case LEFT:
+					ship.goWest();
+					break;
+				case UP:
+					ship.goNorth();
+					break;
+				case DOWN:
+					ship.goSouth();
+					break;
+				default:
+					break;
+				}
+				drawShip(p);
+			}
+			
+		});
+	}
+	
 }
--- src/Ship.java
+++ src/Ship.java
@@ -1,4 +1,65 @@
+import java.awt.*;
+import javafx.scene.image.*;
+import javafx.scene.image.Image;
 
 public class Ship {
 
+	private Point location;
+	private Image img = new Image("file:src/ship.png", 50, 50, true, true);
+	private ImageView imgv = new ImageView (img);
+	private int scale;
+	
+	// Constructor
+	public Ship(int scale) {
+		location = new Point(0, 0);
+		this.scale = scale;
+	}
+	
+	// Returns the location of the ship
+	public Point getShipLocation() {
+		return location;
+	}
+	
+	// Loads the ship image at the specified location
+	public ImageView loadShipImage() {
+		imgv.setX(location.x*scale);
+		imgv.setY(location.y*scale);
+		return imgv;
+	}
+	
+	// Decrements the ship's y coordinate by 1
+	public void goNorth() {
+		int x = location.x;
+		int y = location.y - 1;
+		moveTo (new Point(x, y));
+	}
+	
+	// Increments the ship's y coordinate by 1
+	public void goSouth() {
+		int x = location.x;
+		int y = location.y + 1;
+		moveTo (new Point(x, y));
+	}
+	
+	// Increments the ship's x coordinate by 1
+	public void goEast() {
+		int x = location.x + 1;
+		int y = location.y;
+		moveTo (new Point(x, y));
+	}
+	
+	// Decrements the ship's x coordinate by 1
+	public void goWest() {
+		int x = location.x - 1;
+		int y = location.y;
+		moveTo (new Point(x, y));
+	}
+	
+	private void moveTo (Point p) {
+		if (p.x >= 0 && p.x <= 9 && p.y >= 0 && p.y <= 9) {
+			location = p;
+			imgv.setX(location.getX() * scale);
+			imgv.setY(location.getY() * scale);
+		}
+	}
 }
